a1515811dcb2ac377598b61f72768a8f02698711
miya0001 <miya@wpist.me>
2017-09-13 05:33:04 +0900

    add php 5.3 and later

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # docker-phpenv
+
+[![Build Status](https://travis-ci.org/miya0001/docker-phpenv.svg?branch=master)](https://travis-ci.org/miya0001/docker-phpenv)
+
--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker pull ubuntu:14.04
 docker build -t phpenv .
 docker run -idt --name phpenv --privileged --volume="$(pwd)":/shared/:rw phpenv "/bin/bash"
