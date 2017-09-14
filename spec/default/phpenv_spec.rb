92039732bbbd4bed085f3f27dbfa6c44a3bd017c
miya0001 <miya@wpist.me>
2017-09-14 17:12:41 +0900

    phpunit throw 2 when --version

--- spec/default/phpenv_spec.rb
+++ spec/default/phpenv_spec.rb
@@ -9,9 +9,5 @@ describe command('eval "$(phpenv init -)" && php --version') do
 end
 
 describe command('eval "$(phpenv init -)" && phpunit --version') do
-  its(:exit_status) { should eq 0 }
-end
-
-describe command('eval "$(phpenv init -)" && phpunit --version') do
-  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /PHPUnit .+ by Sebastian Bergmann./ }
 end
